fc46ceee178a9b33362dbecf1201e633b4426043
Bratan Sergey <bsp303@gmail.com>
2022-06-27 19:41:47 +0600

    Изменил таймаут

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_control_button_shopping_cart(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     button = browser.find_elements(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button, 'Отсутствует кнопка добавления в корзину на странице'
